66f39ecbc30c5c3851a3f048d222effbfb423956
Arden de Raaij <a.de.raaij@gmail.com>
2017-10-15 22:08:06 +0100

    [add] deploy sftp config

--- gulpfile.js/tasks/deploySFTP.js
+++ gulpfile.js/tasks/deploySFTP.js
@@ -3,16 +3,16 @@ const plugins = require('gulp-load-plugins')();
 const config = require('../config/index');
 
 // Rename the .ftppass-example.json file to .ftppass.json and change the referred name here
-const ftppass = require('../../.ftppass-example');
+const ftppass = require('../../.ftppass.json');
 
 const sftp = () =>
-    gulp.src(`${config.buildPath}**/*`)
+    gulp.src(`${config.themePath}**/*`)
         .pipe(plugins.sftp({
             host: ftppass.host,
             user: ftppass.user,
             pass: ftppass.pass,
             port: ftppass.sftpPort,
-            remotePath: ftppass.remotePath,
+            remotePath: `${ftppass.remotePath}content/themes/${config.themeName}/`,
         }));
 
 gulp.task('deploy:sftp', ['build:production'], sftp);
