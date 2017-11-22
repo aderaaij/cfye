f6bf2eba6ae4cb9025d504fc7f204b63b04d2d42
Arden de Raaij <a.de.raaij@gmail.com>
2017-11-22 15:09:12 +0000

    [fix] replace arrow function with normal function

--- gulpfile.js/lib/errorHandler.js
+++ gulpfile.js/lib/errorHandler.js
@@ -1,10 +1,8 @@
 const notify = require('gulp-notify');
 const util = require('gulp-util');
 
-const handleErrors = (errorObject, callback) => {
+module.exports = function handleErrors(errorObject) {
     notify.onError(errorObject.toString().split(': ').join(':\n')).apply(this, arguments);
     // Keep gulp from hanging on this task
     if (typeof this.emit === 'function') this.emit('end');
 };
-
-module.exports = handleErrors;
