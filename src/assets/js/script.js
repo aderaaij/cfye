3747b08fd5a820bea9b89977a28b67c74511be1b
Arden de Raaij <a.de.raaij@gmail.com>
2017-11-22 16:27:07 +0000

    [add] popmotion normal

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,28 +1,7 @@
 import 'intersection-observer';
 import './partials/Header';
 import './partials/barbaConfig';
-import { getSiblings } from './partials/helpers';
 
-
-function articleFocus(e) {
-    if (e.key === 'Tab') {
-        const el = document.activeElement;
-        const siblings = getSiblings(el.parentElement);
-        siblings
-            .filter(item => item.tagName === 'ARTICLE')
-            .forEach((article) => {
-                article.classList.add('is-inActive');
-                el.parentElement.classList.remove('is-inActive');
-            });
-    }
-}
-window.addEventListener('keydown', (e) => {
-    articleFocus(e);
-});
-
-window.addEventListener('keyup', (e) => {
-    articleFocus(e);
-});
 
 if ('serviceWorker' in navigator) {
     // register the Service Worker, must be in the root directory to have site-wide scope...
